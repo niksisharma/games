cb4e8176258d165e3b7141aaeb4a9cd778053bb7
Nikita Sharma <51536421+niksisharma@users.noreply.github.com>
2021-10-01 23:05:10 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,8 +1,12 @@
 def display_board(board):
 
-	print(board[7]+'|'+board[8]+'|'+board[9])
-	print(board[4]+'|'+board[5]+'|'+board[6])
-	print(board[1]+'|'+board[2]+'|'+board[3])
+    print("*-----*-----*-----*")
+    print(f"|  {board[7]}  |  {board[8]}  |  {board[9]}  |")
+    print("*-----*-----*-----*")
+    print(f"|  {board[4]}  |  {board[5]}  |  {board[6]}  |")
+    print("*-----*-----*-----*")
+    print(f"|  {board[1]}  | {board[2]}   |  {board[3]}  |")
+    print("*-----*-----*-----*")
 
 def player_input():
     marker = ''

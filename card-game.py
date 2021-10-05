1979a8a1793f33004bd5484bed9fd03ac3211e61
Nikita Sharma <51536421+niksisharma@users.noreply.github.com>
2021-10-05 16:38:33 +0530

    Update and rename card game.py to card-game.py

--- card-game.py
+++ card-game.py
@@ -7,7 +7,7 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
 
 class Card:
 
-	def __init__(self,suit,rank):
+	def __init__(self,suite,rank):
 		self.suite=suite
 		self.rank=rank
 		self.value=values[rank]
